8d2b9301bbab173dbcdad629017187746bb759db
Camio1945 <camio1945@qq.com>
2022-03-09 09:05:52 +0800

    程序3.19：图的邻接表表示

--- Chapter03/program_03_19/program_03_19.edited.c
+++ Chapter03/program_03_19/program_03_19.edited.c
@@ -22,31 +22,33 @@ link NEW(int v, link next) {
 
 main() {
   setbuf(stdout, NULL); // 将标准输出的缓存区清零，防止printf不及时打印
+  printf("本程序需要在控制台输入若干行数字(0到7之间的整数)，每行两个数字(数字间用空格隔开)，用于表示两个点是连通的，如：\n"
+         "0 1\n"
+         "0 2\n"
+         "0 5\n"
+         "0 6\n"
+         "0 7\n"
+         "1 7\n"
+         "2 7\n"
+         "3 4\n"
+         "3 5\n"
+         "4 5\n"
+         "4 6\n"
+         "4 7\n"
+  );
+  printf("提示：控制台输入完成后，按回车，然后按快捷键：Ctrl + D （也可能是Ctrl + C 或 Ctrl + Z，不同平台不一样，多试试）\n");
   int i, j, V = 8;
   link adj[V];
+  // for循环作用：初始化一维数组adj
   for (i = 0; i < V; i++) {
     adj[i] = NULL;
   }
-  int arr2d[12][2] = {
-      {0, 1},
-      {0, 2},
-      {0, 5},
-      {0, 6},
-      {0, 7},
-      {1, 7},
-      {2, 7},
-      {3, 4},
-      {3, 5},
-      {4, 5},
-      {4, 6},
-      {4, 7},
-  };
-  for (int k = 0; k < 12; ++k) {
-    i = arr2d[k][0];
-    j = arr2d[k][1];
+  // while循环作用：把输入的两个数关联到对应的链表上
+  while (scanf("%d %d\n", &i, &j) == 2) {
     adj[i] = NEW(j, adj[i]);
     adj[j] = NEW(i, adj[j]);
   }
+  // for循环作用：打印结果
   for (int k = 0; k < V; ++k) {
     link obj = adj[k];
     printf("连接到节点 %d 的节点有：", k);
