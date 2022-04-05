7426e62364bec49751fa31cb57631204b769b6a1
erhu1999 <837087787@qq.com>
2022-04-05 21:49:19 +0800

    程序5.4：前缀表达式求值的递归程序

---
+++ Chapter05/program_05_04/program_05_04.edited.c
@@ -0,0 +1,34 @@
+/** 程序5.4：前缀表达式求值的递归程序 */
+#include <stdio.h>
+
+//char *a = "* + 7 * * 4 6 + 8 9 5";
+char *a = "* 4 + 2 3";
+int i = 0;
+
+/** 前缀表达式求值 */
+int eval() {
+  while (a[i] == ' ') {
+    i++;
+  }
+  if (a[i] == '+') {
+    i++;
+    return eval() + eval();
+  }
+  if (a[i] == '*') {
+    i++;
+    return eval() * eval();
+  }
+  int x = 0;
+  while (a[i] >= '0' && a[i] <= '9') {
+    x = 10 * x + (a[i] - '0');
+    i++;
+  }
+  return x;
+}
+
+
+main() {
+  setbuf(stdout, NULL);
+  printf("前缀表达式为：%s\n", a);
+  printf("前缀表达式的值为：%d", eval());
+}
